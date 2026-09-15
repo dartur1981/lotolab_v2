0f1a4c2f1c654a81821a1bc1bae36c410ee4a9e0
Daniel Fonseca <dartur1981@gmail.com>
2026-09-15 17:02:47 -0300

    fix(proxy): confia apenas em FOR e PROTO para preservar porta 8081 nos redirecionamentos

--- web/bootstrap/app.php
+++ web/bootstrap/app.php
@@ -15,9 +15,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->trustProxies(
             at: '*',
             headers: Request::HEADER_X_FORWARDED_FOR |
-                     Request::HEADER_X_FORWARDED_PORT |
-                     Request::HEADER_X_FORWARDED_PROTO |
-                     Request::HEADER_X_FORWARDED_AWS_ELB
+                     Request::HEADER_X_FORWARDED_PROTO
         );
     })
     ->withExceptions(function (Exceptions $exceptions): void {
