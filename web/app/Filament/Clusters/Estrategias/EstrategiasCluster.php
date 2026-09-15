bd1aecb2697388840dcaa57b09c2290af4a4aa57
Daniel Fonseca <dartur1981@gmail.com>
2026-09-15 16:33:37 -0300

    fix(navigation): corrige link do cluster de estrategias e headers vazios do FastCGI

--- web/app/Filament/Clusters/Estrategias/EstrategiasCluster.php
+++ web/app/Filament/Clusters/Estrategias/EstrategiasCluster.php
@@ -5,6 +5,7 @@ namespace App\Filament\Clusters\Estrategias;
 use BackedEnum;
 use Filament\Clusters\Cluster;
 use Filament\Support\Icons\Heroicon;
+use Illuminate\Database\Eloquent\Model;
 
 class EstrategiasCluster extends Cluster
 {
@@ -13,4 +14,20 @@ class EstrategiasCluster extends Cluster
     protected static ?int $navigationSort = 100;
     protected static ?string $navigationLabel = 'Estratégias';
     protected static ?string $slug = 'estrategias';
+
+    public static function getNavigationUrl(): string
+    {
+        return \App\Filament\Clusters\Estrategias\Pages\Estrategias::getUrl();
+    }
+
+    public static function getUrl(array $parameters = [], bool $isAbsolute = true, ?string $panel = null, ?Model $tenant = null): string
+    {
+        return \App\Filament\Clusters\Estrategias\Pages\Estrategias::getUrl($parameters, $isAbsolute, $panel, $tenant);
+    }
+
+    public function mount(): void
+    {
+        $this->redirect(\App\Filament\Clusters\Estrategias\Pages\Estrategias::getUrl(), navigate: false);
+    }
 }
+
